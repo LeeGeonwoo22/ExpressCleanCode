9530fa4cc4c47bcfba7b7d8656f4b55e5b5a8449
LeeGeonwoo22 <kwoo.chris@gmail.com>
2022-07-03 20:39:59 +0900

    [Study] 회원가입/로그인 기능 만들기 #5

--- src/app.js
+++ src/app.js
@@ -6,14 +6,12 @@ const morgan = require('morgan')
 const {logger}  = require('./config/winston')
 require('dotenv').config()
 
-const Port =  process.env.DB_PORT
-const User =  process.env.DB_USER
-const Password = process.env.DB_PASSWORD
+const PORT =  process.env.DB_PORT
+const USER =  process.env.DB_USER
+const PASSWORD = process.env.DB_PASSWORD
+const { User } = require('./entities/models/User')
 
-
-mongoose.connect(`mongodb+srv://${User}:${Password}@cluster0.cvbef.mongodb.net/?retryWrites=true&w=majority`).then(() => console.log('MongoDb Connected...')).catch(err=> console.log(err))
-
-
+mongoose.connect(`mongodb+srv://${USER}:${PASSWORD}@cluster0.cvbef.mongodb.net/?retryWrites=true&w=majority`).then(() => console.log('MongoDb Connected...')).catch(err=> console.log(err))
 
 morgan('combined', {
     skip: function (req, res) { return res.statusCode < 400 }
@@ -23,19 +21,34 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    logger.info('GET /');
+    // logger.info('GET /');
   res.send('Hello World!')
 //   res.sendStatus(200);
 })
 
 
-app.get('/error', (req, res) => {
-    logger.error('Error message');
-  res.sendstatus(400);
+// app.get('/error', (req, res) => {
+//     logger.error('Error message');
+//   res.sendstatus(400);
+// })
+
+// 회원가입
+app.post('/signup', (req,res)=>{
+  const user = new User(req.body)
+  user.save((err, doc) =>{
+    if(err) return res.json(
+      {
+        success : false , err
+      }
+    )
+      return res.status(200).json({
+        success : true
+      })
+  })
 })
 
-app.listen(Port, () => {
-  console.log(`Example app listening on port ${Port}`)
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
 })
 
 
